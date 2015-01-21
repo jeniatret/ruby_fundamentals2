1734b13e7d3960373d1ad216f7e694568ae5d1c9
Jenia Tretiyakova <zhanyat@gmail.com>
2015-01-21 14:49:54 -0500

    I added rice to my original list and created a method that simplifies the printing part

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,17 @@
-grocery_list = ["carrots", "apples", "chips", "bread"]
+def store(grocery_list)
 
 #grocery list with an item on each line with an * in front of it
 grocery_list.each {|item| puts "* #{item}"}
+
+end
+
+#original line 
+grocery_list = ["carrots", "apples", "chips", "bread"]
+store(grocery_list)
+
+puts "-----------------------"	
+
+#add rice to the list and output it in the list
+grocery_list << "rice"
+store(grocery_list)
+
